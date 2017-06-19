3c343ff6cabc519baec279d5a17828dcfc6cfbee
fanggang.wang <fanggang.wang@hpe.com>
2017-06-19 13:23:17 +0800

    Add Junit test

---
+++ src/test/java/com/springdemo/test/DemoTest.java
@@ -0,0 +1,25 @@
+package com.springdemo.test;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations= {"classpath:applicationContext.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DemoTest {
+	
+	@Test
+	public void successTest(){
+		assertTrue(true);
+	}
+	
+	@Test
+	public void failureTest(){
+		assertTrue(false);
+	}
+
+}
