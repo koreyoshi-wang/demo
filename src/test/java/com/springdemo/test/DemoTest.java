81a56fb515395780faadb194a22a2b775f79fc5b
fanggang.wang <fanggang.wang@hpe.com>
2017-06-19 15:30:32 +0800

    Fixed bug on jenkins

--- src/test/java/com/springdemo/test/DemoTest.java
+++ src/test/java/com/springdemo/test/DemoTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(locations= {"classpath:applicationContext.xml"})
+@ContextConfiguration(locations= {"classpath:spring-servlet.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DemoTest {
 	
