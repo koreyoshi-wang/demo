536a5cd6268532cb9744e5d23b535f28e44b2b58
hou-dong.liu <hou-dong.liu@hpe.com>
2017-09-21 10:51:52 +0800

    test

--- src/test/java/com/springdemo/test/DemoTest.java
+++ src/test/java/com/springdemo/test/DemoTest.java
@@ -20,7 +20,7 @@ public class DemoTest {
 	
 	@Resource
 	private LoginServiceImpl loginServiceImpl;
-	String userName = "test@hpe.com";
+	String userName = "aaa@hpe.com";
 	
 	@Test
 	public void verifyUser() {
@@ -35,6 +35,26 @@ public class DemoTest {
 	
 	@Test
 	public void test03() {
+		Assert.assertEquals(true, 3 == 4);
+	}
+	
+	@Test
+	public void test04() {
+		Assert.assertEquals(true, 1 == 2);
+	}
+	
+	@Test
+	public void test05() {
+		Assert.assertEquals(true, 3 == 4);
+	}
+	
+	@Test
+	public void test06() {
+		Assert.assertEquals(true, 1 == 2);
+	}
+	
+	@Test
+	public void test07() {
 		Assert.assertEquals(true, 3 == 4);
 	}
 }
